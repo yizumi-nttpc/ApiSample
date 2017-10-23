9798f7ba9df5419ce1bb332085b4e5ba5d4b913e
泉　陽子 <yizumi@izumiyoushinoMacBook-Air.local>
2017-10-23 23:21:34 +0900

    DIVE14-2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blogs, only: [:index]
 
-  resources :poems, only: [:index]
+  resources :poems, only: [:index, :show]
 
 end
